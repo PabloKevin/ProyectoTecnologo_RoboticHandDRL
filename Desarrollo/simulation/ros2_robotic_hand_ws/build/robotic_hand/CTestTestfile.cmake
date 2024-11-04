7309cf92b3a51dd339d493687db0ddfda210e192
PabloKevin <pablo.pereira.p@estudiantes.utec.edu.uy>
2024-11-04 13:29:59 -0300

    Estado actual 4/11/2024

---
+++ Desarrollo/simulation/ros2_robotic_hand_ws/build/robotic_hand/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pablo_kevin/ros2_robotic_hand_ws/src/robotic_hand
+# Build directory: /home/pablo_kevin/ros2_robotic_hand_ws/build/robotic_hand
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
