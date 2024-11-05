b94794d1f6c2bc57708fdf7f56029dff38af5b3e
PabloKevin <pablo.pereira.p@estudiantes.utec.edu.uy>
2024-11-05 20:31:47 -0300

    probando

--- Desarrollo/simulation/ros2_robotic_hand_ws/install/robotic_hand/share/robotic_hand/launch/robotic_hand_gazebo.launch.py
+++ Desarrollo/simulation/ros2_robotic_hand_ws/install/robotic_hand/share/robotic_hand/launch/robotic_hand_gazebo.launch.py
@@ -1,42 +1,61 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
 
-# Obtener la ruta de instalación del paquete
-packagepath = get_package_share_directory('robotic_hand')  # Cambiar 'nav_car' por 'robotic_hand'
-print(packagepath)
-print(os.path.dirname(__file__))
+# Get package path
+packagepath = get_package_share_directory('robotic_hand')
 
-carmodel = packagepath + '/urdf/robotic_hand.sdf'  # Actualizar la ruta al modelo
-robot_desc = open(carmodel).read()
+# File paths
+sdf_model_path = os.path.join(packagepath, 'urdf', 'ros2_control', 'gazebo', 'robotic_hand.urdf.xacro')
+world_file_path = os.path.join(packagepath, 'worlds', 'empty.sdf')
+config_file_path = os.path.join(packagepath, 'config', 'hand_controllers.yaml')
+
+robot_desc = open(sdf_model_path).read()
 
 def generate_launch_description():
-    gazebo_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(
-            get_package_share_directory('ros_gz_sim'), 'launch',
-            'gz_sim.launch.py')),
-        launch_arguments={'gz_args': packagepath + '/worlds/empty.sdf -r -v4'}.items()  # Cambiar a tu archivo de mundo
+    # Launch arguments
+    declare_use_sim_time = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='true',
+        description='Use simulation time if true'
     )
 
-    robot_to_gazebo = Node(
+    declare_world = DeclareLaunchArgument(
+        name='world',
+        default_value=world_file_path,
+        description='Full path to the world file'
+    )
+
+    # Gazebo launch
+    start_gazebo_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
+        )),
+        launch_arguments={'gz_args': [world_file_path, ' -r -v4']}.items()
+    )
+
+    # Robot to Gazebo
+    spawn_robot_cmd = Node(
         package='ros_gz_sim',
         executable='create',
         arguments=['-string', robot_desc, '-x', '0', '-y', '0', '-z', '0.05', '-name', 'robotic_hand']
     )
 
+    # ROS 2 bridge node
     bridge_node = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        parameters=[{'config_file': packagepath + '/config/hand_controllers.yaml'}]  # Cambiar a la ruta correcta del archivo de configuración
+        parameters=[{'config_file': config_file_path}]
     )
 
-    robot_desc_node = Node(
+    # Robot state publisher
+    robot_state_publisher_cmd = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
@@ -47,9 +66,35 @@ def generate_launch_description():
         ]
     )
 
-    return LaunchDescription([
-        gazebo_node,
-        robot_to_gazebo,
-        bridge_node,
-        robot_desc_node,
-    ])
+    # Arm controller
+    load_arm_controller_cmd = Node(
+        package='controller_manager',
+        executable='spawner.py',
+        arguments=['arm_controller'],
+        output='screen'
+    )
+
+    # Gripper controller
+    load_gripper_controller_cmd = Node(
+        package='controller_manager',
+        executable='spawner.py',
+        arguments=['grip_controller'],
+        output='screen'
+    )
+
+    # Launch description
+    ld = LaunchDescription()
+
+    # Declare launch arguments
+    ld.add_action(declare_use_sim_time)
+    ld.add_action(declare_world)
+
+    # Add actions
+    ld.add_action(start_gazebo_cmd)
+    ld.add_action(spawn_robot_cmd)
+    ld.add_action(bridge_node)
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(load_arm_controller_cmd)
+    ld.add_action(load_gripper_controller_cmd)
+
+    return ld
