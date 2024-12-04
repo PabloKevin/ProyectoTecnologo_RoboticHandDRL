6897ade54cea5089d81cdc5d645ceb82916a43a1
PabloKevin <pablo.pereira.p@estudiantes.utec.edu.uy>
2024-12-04 06:16:42 -0300

    Part 4 otra mitad

--- Desarrollo/simulation/Antecedentes/RoboticArmManipulation_RL/td3-robosuite-door/td3_torch.py
+++ Desarrollo/simulation/Antecedentes/RoboticArmManipulation_RL/td3-robosuite-door/td3_torch.py
@@ -7,6 +7,7 @@ from buffer import ReplayBuffer
 from networks import ActorNetwork, CriticNetwork
 
 class Agent:
+    # Va a estar compuesto en total por 6 redes neuronales
     def __init__(self, actor_learning_rate, critic_learning_rate, input_dims, tau, env, gamma=0.99, update_actor_interval=2, warmup=1000, 
                  n_actions=2, max_size=1000000, layer1_size=256, layer2_size=128, batch_size=100, noise=0.1):
 
@@ -77,3 +78,67 @@ class Agent:
 
         # manda el mu_prime a la cpu, lo transforma en un tensor, y luego lo transforma en un valor np para leer la acción y ejecutarla o mandarla a otro lado.
         return mu_prime.cpu().detach().numpy()  
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.store_transition(state, action, reward, next_state, done)
+
+    def learn(self):
+        if self.memory.mem_ctr < self.batch_size * 10:
+            return
+
+        state, action, reward, next_state, done = self.memory.sample_buffer(self.batch_size)
+
+        reward = T.tensor(reward, dtype=T.float).to(self.critic_1.device)
+        done = T.tensor(done).to(self.critic_1.device)
+        next_state = T.tensor(next_state, dtype=T.float).to(self.critic_1.device)
+        state = T.tensor(state, dtype=T.float).to(self.critic_1.device)
+        action = T.tensor(action, dtype=T.float).to(self.critic_1.device)
+
+        target_actions = self.target_actor.forward(next_state)
+        target_actions = target_actions + T.clamp(T.tensor(np.random.normal(scale=0.2)), -0.5, 0.5)
+        target_actions = T.clamp(target_actions, self.min_action[0], self.max_action[0])
+
+        next_q1 = self.target_critic_1.forward(next_state, target_actions)
+        next_q2 = self.target_critic_2.forward(next_state, target_actions)
+        
+        q1 = self.critic_1.forward(state, action)
+        q2 = self.critic_2.forward(state, action)
+
+        next_q1[done] = 0.0
+        next_q2[done] = 0.0
+
+        next_q1 = next_q1.view(-1)
+        next_q2 = next_q2.view(-1)
+
+        next_critic_value = T.min(next_q1, next_q2)
+        target = reward + self.gamma * next_critic_value
+        target = target.view(self.batch_size, 1)
+
+        self.critic_1.optimizer.zero_grad()
+        self.critic_2.optimizer.zero_grad()
+
+        q1_loss = F.mse_loss(target, q1)
+        q2_loss = F.mse_loss(target, q2)
+
+        critic_loss = q1_loss + q2_loss
+        critic_loss.backward()
+
+        self.critic_1.optimizer.step()
+        self.critic_2.optimizer.step()
+
+        self.learn_step_cntr += 1
+
+        if self.learn_step_cntr % self.update_actor_iter != 0:
+            return
+
+        self.actor.optimizer.zero_grad()
+        actor_q1_loss = self.critic_1.forward(state, self.actor.forward(state))
+        actor_loss = -T.mean(actor_q1_loss)
+        actor_loss.backward()
+
+        self.actor.optimizer.step()
+        self.update_networks_parameters()
+
+    
+    def update_networks_parameters(self, tau):
+        pass
