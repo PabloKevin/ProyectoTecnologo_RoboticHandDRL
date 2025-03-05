f7386b2a05ad20b6a8a932976bc4a80f8419c2ac
PabloKevin <pablo.pereira.p@estudiantes.utec.edu.uy>
2025-03-05 03:05:14 -0300

    creación de run_all.sh, para poder ejecutar las 4 terminales y todas sus correspondientes líneas de comando de una sola vez.

---
+++ run_all.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+gnome-terminal --tab --title="ROS RViz" -- bash -ic "
+  echo '=== ROS RViz Terminal ===';
+  cd Desarrollo/simulation/ros2_robotic_hand_ws/;
+  source install/setup.bash;
+  ros2 launch robotic_hand ros_rviz.launch.py;
+  exec bash
+"
+
+gnome-terminal --tab --title="Image Generator" -- bash -ic "
+  echo '=== Image Generator Terminal ===';
+  cd Desarrollo/simulation/Env03/;
+  conda activate RoboticHand_ML;
+  python3 Image_generator_server.py;
+  exec bash
+"
+
+gnome-terminal --tab --title="Agent Server" -- bash -ic "
+  echo '=== Agent Server Terminal ===';
+  cd Desarrollo/simulation/Env03/;
+  conda activate RoboticHand_ML;
+  python3 Agent_server.py;
+  exec bash
+"
+
+gnome-terminal --tab --title="Agent Controller" -- bash -ic "
+  echo '=== Agent Controller Terminal ===';
+  cd Desarrollo/simulation/ros2_robotic_hand_ws/;
+  source install/setup.bash;
+  conda deactivate;
+  echo 'Waiting 5 seconds for other processes...';
+  sleep 10;  # <-- Wait 10 seconds here;
+  ros2 run robotic_hand agent_controller.py;
+  echo 'last command: ros2 run robotic_hand agent_controller.py';
+  exec bash
+"
