aa480830d3bd5c2ad87bc81e050d8d7a32065dbf
PabloKevin <pablo.pereira.p@estudiantes.utec.edu.uy>
2025-04-14 04:12:38 -0300

    terminando clase model_performance. Faltan detalles finales.

--- Desarrollo/simulation/Env03/Data_Visualization/model_performance.py
+++ Desarrollo/simulation/Env03/Data_Visualization/model_performance.py
@@ -25,25 +25,30 @@ class Model_Metrics():
         
         self.true_labels, self.pred_labels = self.pred_labels2classes()
         self.binary_true_labels, self.binary_pred_labels = self.calculate_binary_labels()
+        self.pred_scores = self.calculate_predScores()
+        self.bool_true_labels = self.calculate_bool_labels()
+
+        self.f1, self.precision_val, self.recall_val, self.accuracy = self.calculate_metrics(show=False)
 
     def pred_labels2classes(self):
         self.true_labels = self.get_class_from_reg(self.df_test["true_label"], self.thresholds)
         self.pred_labels = self.get_class_from_reg(self.df_test["predicted_label"], self.thresholds)
         return self.true_labels, self.pred_labels
 
-    def calculate_binary_labels(self):
-        self.pred_labels2classes()
+    def calculate_binary_labels(self, first_update=False):
+        if first_update:
+            self.pred_labels2classes()
         binary_true_labels = []
         binary_pred_labels = []
-        for i in range(len(np.unique(true_labels))): #-1 por el default
+        for i in range(len(np.unique(self.true_labels))): #-1 por el default
             true_lab = []
             pred_lab = []
-            for j,label in enumerate(true_labels):
+            for j,label in enumerate(self.true_labels):
                 if label == i:
                     true_lab.append(1)
                 else:
                     true_lab.append(0)
-                if pred_labels[j] == i:
+                if self.pred_labels[j] == i:
                     pred_lab.append(1)
                 else:
                     pred_lab.append(0)
@@ -54,10 +59,13 @@ class Model_Metrics():
         self.binary_pred_labels = binary_pred_labels
         return binary_true_labels, binary_pred_labels
 
-    def get_class_from_reg(self, reg, thresholds = [0.5, 1.6+0.5, 3.2+0.5, float("inf")]):
+    def get_class_from_reg(self, reg, thresholds=None):
         """
         Convert a regression value to a class label.
         """
+        if thresholds is None:
+            thresholds = self.thresholds
+
         class_names = []
         for r in reg:
             for i, threshold in enumerate(thresholds):
@@ -66,7 +74,7 @@ class Model_Metrics():
                     break
         return class_names
 
-    def plot_confusion_matrix(self, y_true, y_pred, class_names, thresholds, model_name=""):
+    def plot_confusion_matrix(self):
         """
         Plots a confusion matrix using the true labels and predictions.
 
@@ -77,13 +85,13 @@ class Model_Metrics():
         """
         #plt.figure()
         fig, ax = plt.subplots(figsize=(9, 9)) 
-        cm = confusion_matrix(y_true, y_pred)
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
+        cm = confusion_matrix(self.true_labels, self.pred_labels)
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=self.class_names)
         disp.plot(cmap=plt.cm.Reds, ax=ax)
         plt.xticks(rotation=45)
-        plt.title(f"Confusion Matrix\nmodel: {model_name}")
+        plt.title(f"Confusion Matrix\nmodel: {self.model_name}")
 
-        info_text = f"thresholds: {thresholds}"
+        info_text = f"thresholds: {self.thresholds}"
         plt.gcf().text(
             0.2, 0.04, 
             info_text,
@@ -95,15 +103,23 @@ class Model_Metrics():
         )
         plt.show()
 
-    def plot_ROC_curve(self, y_true, y_pred, class_names, thresholds, model_name=""):
-        # ROC Curve
+    def calculate_predScores(self):
         # Normalize your continuous prediction to 0–1 range for ROC
-        pred_scores = np.abs(df_test["predicted_label"] - df_test["true_label"])
+        pred_scores = np.abs(self.df_test["predicted_label"] - self.df_test["true_label"])
         pred_scores = 1 - (pred_scores / pred_scores.max())  # Closer to target class gets higher score
-
-        bool_true_lables = np.array(true_labels) == np.array(pred_labels)
-
-        fpr, tpr, _ = roc_curve(bool_true_lables, pred_scores)
+        self.pred_scores = pred_scores
+        return self.pred_scores
+    
+    def calculate_bool_labels(self):
+        self.bool_true_labels = np.array(self.true_labels) == np.array(self.pred_labels)
+        return self.bool_true_labels
+        
+    def plot_ROC_curve(self, first_update=False):
+        if first_update:
+            self.calculate_predScores()
+            self.calculate_bool_labels()
+        # ROC Curve
+        fpr, tpr, _ = roc_curve(self.bool_true_labels, self.pred_scores)
         roc_auc = auc(fpr, tpr)
 
         plt.figure(figsize=(10, 5))
@@ -111,15 +127,19 @@ class Model_Metrics():
         plt.plot([0, 1], [0, 1], linestyle='--', color='gray')
         plt.xlabel('False Positive Rate')
         plt.ylabel('True Positive Rate (Recall)')
-        plt.title('ROC Curve\nmodel: {model_name}')
+        plt.title('ROC Curve\nmodel: {self.model_name}')
         plt.legend(loc='lower right')
         plt.grid()
         plt.show()
 
-    def plot_PresicionRecall_curve(self):
+    def plot_PresicionRecall_curve(self, first_update=False):
+        if first_update:
+            self.calculate_predScores()
+            self.calculate_bool_labels()
+
         # Precision-Recall Curve
-        precision, recall, _ = precision_recall_curve(bool_true_lables, pred_scores)
-        average_precision = average_precision_score(bool_true_lables, pred_scores)
+        precision, recall, _ = precision_recall_curve(self.bool_true_labels, self.pred_scores)
+        average_precision = average_precision_score(self.bool_true_labels, self.pred_scores)
 
         plt.figure(figsize=(10, 5))
         plt.plot(recall, precision, lw=2, label=f'PR curve (AP = {average_precision:.2f})')
@@ -130,16 +150,50 @@ class Model_Metrics():
         plt.grid()
         plt.show()
 
-    def calculate_metrics(self):
-        # Metrics Calculation
-        f1 = f1_score(true_labels, pred_labels, average="macro")
-        precision_val = precision_score(true_labels, pred_labels, average="macro")
-        recall_val = recall_score(true_labels, pred_labels, average="macro")
+    def plot_predicted_vs_true(self):
+        colors = []
+        for i in range(len(self.df_test["true_label"])):
+            colors.append(np.random.randint(0, 256, 3))
+        
+        dot_color = []
+        for label in self.df_test["true_label"]:
+            for i,th in enumerate(thresholds):
+                if label < th:
+                    dot_color.append(["red", "blue", "green", "gray"][i])
+                    break
+        plt.scatter(self.df_test["predicted_label"], self.df_test["true_label"], color=dot_color, alpha=0.6)
+        plt.title(f"Predicted vs True Labels")
+        plt.xlabel('Predicted Labels')
+        plt.ylabel('True Labels')
+        plt.legend(loc='lower left')
+        plt.grid()
+        plt.show()
 
-        print(f'F1-Score: {f1:.2f}')
-        print(f'Precision: {precision_val:.2f}')
-        print(f'Recall: {recall_val:.2f}')
-        print(f'Accuracy: {np.mean(np.array(true_labels) == np.array(pred_labels)):.2f}')
+    def calculate_metrics(self, first_update=False, show=True):
+        if first_update:
+            self.calculate_predScores()
+            self.calculate_bool_labels()
+
+        # Metrics Calculation
+        self.f1 = f1_score(self.true_labels, self.pred_labels, average="macro")
+        self.precision_val = precision_score(self.true_labels, self.pred_labels, average="macro")
+        self.recall_val = recall_score(self.true_labels, self.pred_labels, average="macro")
+        self.accuracy = np.mean(np.array(self.true_labels) == np.array(self.pred_labels))
+
+        if show:
+            print(f'F1-Score: {self.f1:.2f}')
+            print(f'Precision: {self.precision_val:.2f}')
+            print(f'Recall: {self.recall_val:.2f}')
+            print(f'Accuracy: {self.accuracy:.2f}')
+
+        return self.f1, self.precision_val, self.recall_val, self.accuracy
+    
+    def show_model_performance(self):
+        self.plot_confusion_matrix()
+        self.plot_ROC_curve()
+        self.plot_PresicionRecall_curve()
+        self.plot_predicted_vs_true()
+        self.calculate_metrics()
 
 # Example usage
 if __name__ == "__main__":
@@ -161,19 +215,9 @@ if __name__ == "__main__":
     class_names = ["agarre0", "agarre1", "agarre2", "agarre3"]
     #class_names = ["empty", "tuerca", "tornillo", "clavo", "lapicera", "tenedor", "cuchara", "destornillador", "martillo", "pinza"]
 
+    observer_performance = Model_Metrics(conv_channels=conv_channels, hidden_layers=hidden_layers, model_weight_dir=model_weight_dir, model_name=model_name, thresholds=thresholds, class_names=class_names)
+    observer_performance.show_model_performance()
 
-    plot_confusion_matrix(true_labels, pred_labels, class_names, thresholds, model_name)
-    
-    """    
-    colors = []
-    for label in df_test["true_label"]:
-        for i,th in enumerate(thresholds):
-            if label < th:
-                colors.append(["red", "blue", "green", "gray"][i])
-                break
-    plt.scatter(df_test["predicted_label"], df_test["true_label"], color=colors)
-    plt.show()
-    """
     
 
     
